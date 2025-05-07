096f02494ed2b5f3ccf81d67caf844701fa212a6
Jade Ruthven <123754076+sjruthven@users.noreply.github.com>
2025-05-07 18:28:38 +0100

    Update JsonHelper.cs

--- Assets/Scripts/Utilities/JsonHelper.cs
+++ Assets/Scripts/Utilities/JsonHelper.cs
@@ -3,7 +3,7 @@ using UnityEngine;
 
 public static class JsonHelper
 {
-    // serialises an object to a JSON string
+    // serialisee an object to a JSON string
     public static string ToJson<T>(T obj)
     {
         return JsonUtility.ToJson(obj);
@@ -15,15 +15,15 @@ public static class JsonHelper
         return JsonUtility.FromJson<T>(json);
     }
 
-    // deserialising a JSON string to a list of objects
+    // deserialises a JSON string to a list of objects
     public static List<T> FromJsonList<T>(string json)
     {
-        // this creates a wrapper class to hold in the list
+        // this create a wrapper class to hold in the list
         Wrapper<T> wrapper = JsonUtility.FromJson<Wrapper<T>>(json);
         return wrapper.Items;
     }
 
-    // the wrapper class for deserialising this lists
+    // the wrapper class for deserialising the lists
     [System.Serializable]
     private class Wrapper<T>
     {
